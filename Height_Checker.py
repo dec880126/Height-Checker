763c1273957938b2e8c00db072299eff67acbb6d
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-06-14 01:02:08 +0800

    Update Height_Checker.py

--- Height_Checker.py
+++ Height_Checker.py
@@ -7,6 +7,7 @@
 # expected: [1,1,1,2,3,4]
 # Indices 2, 4, and 5 do not match.
 
+# Original complement: Runtime = 32 ms ; Memory = 14.1 MB
 class Solution:
     def heightChecker(self, heights: list[int]) -> int:
         excepted = heights.copy()
